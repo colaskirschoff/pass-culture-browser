fd48ee0b2e4735adeb20ad185d1bb06ac85ddd40
erwanledoux <lawanledoux@gmail.com>
2018-01-10 17:19:45 +0100

    fixed conflict

--- src/hocs/withLogin.js
+++ src/hocs/withLogin.js
@@ -7,8 +7,17 @@ import { closeModal, showModal } from '../reducers/modal'
 const withLogin = WrappedComponent => {
   class _withLogin extends Component {
     componentWillMount = () => {
+<<<<<<< HEAD
       const { user, showModal } = this.props
       !user && showModal(<Sign />)
+=======
+      // be sure that user is not defined yet
+      // by waiting a bit
+      setTimeout(() => {
+        const { user, showModal } = this.props
+        !user && showModal(<Sign />)
+      }, 1000)
+>>>>>>> origin/automatic-sign
     }
     componentWillReceiveProps = nextProps => {
       if (nextProps.user && nextProps.user !== this.props.user) {

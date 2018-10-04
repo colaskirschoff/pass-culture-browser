6f4d1e8eda0f59a06d5213c140273c68bbe43ace
Daniel Baudry <dbaudry@octo.com>
2018-10-04 10:12:20 +0200

    add wildcard redirect

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var directory = '/' + (process.env.STATIC_DIR || 'dist')
 app.use(express.static(__dirname + directory));
-app.get('/*', (req,res) => res.sendFile(path.join(__dirname+directory+'index.html')))
+app.get('/*', (req,res) => res.sendFile(path.join(__dirname+directory+'/index.html')))
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {

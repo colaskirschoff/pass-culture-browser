f729b83beedd542423d1878578392eea7dbd6324
sixertoy <sixertoy.github@gmail.com>
2018-10-11 09:08:59 +0200

    💄 [PROFILE] update de la vue MonPassCulture

--- src/components/pages/profile/MonPassCulture.js
+++ src/components/pages/profile/MonPassCulture.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const jaugeHeight = 34
 const jaugePadding = 12
@@ -24,23 +25,30 @@ const getPercent = (expense, scale) => {
   return percent * scale
 }
 
-const MonPassCulture = ({ user }) => {
-  const { expenses } = user
-  let scale = 1
-  // TODO si le pourentage des jauges digital/physical est en dessous
-  // de la globale alors il faut faire en sorte que le pourcentage des juages physical/digital soit égal à celui de la globale
-  const percentOverall = getPercent(expenses.all, scale)
-  //
-  scale = expenses.physical.max / expenses.all.max
-  const percentPhysical = getPercent(expenses.physical, scale)
-  //
-  scale = expenses.digital.max / expenses.all.max
-  const percentDigital = getPercent(expenses.digital, scale)
-  return (
-    <div id="mon-pass-culture">
-      <h3 className="dotted-bottom-primary is-primary-text is-uppercase pb8 px12">
-        <i>Mon PassCulture</i>
-      </h3>
+class MonPassCulture extends React.PureComponent {
+  renderActivationLink = () => (
+    <div className="mb16 mt18 text-center">
+      <Link to="/activation" className="is-block fs18 px24 py8">
+        <span className="is-block">Active votre</span>
+        <span className="is-block">porte-monnaie numérique</span>
+      </Link>
+    </div>
+  )
+
+  renderWalletJauges = () => {
+    const { user } = this.props
+    const { expenses } = user
+    let scale = 1
+    // TODO si le pourentage des jauges digital/physical est en dessous
+    // de la globale alors il faut faire en sorte que le pourcentage des juages physical/digital soit égal à celui de la globale
+    const percentOverall = getPercent(expenses.all, scale)
+    //
+    scale = expenses.physical.max / expenses.all.max
+    const percentPhysical = getPercent(expenses.physical, scale)
+    //
+    scale = expenses.digital.max / expenses.all.max
+    const percentDigital = getPercent(expenses.digital, scale)
+    return (
       <div id="wallet-jauges" className="jauges padded">
         <div className="text overall flex-1">
           <b className="is-block">Il reste {expenses.all.actual} €</b>
@@ -80,8 +88,24 @@ const MonPassCulture = ({ user }) => {
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  render() {
+    const { user } = this.props
+    const isWalletActivated = user.wallet_is_activated
+    return (
+      <div id="mon-pass-culture">
+        <h3 className="dotted-bottom-primary pb8 px12">
+          <span className="is-italic is-uppercase is-primary-text">
+            Mon PassCulture
+          </span>
+        </h3>
+        {isWalletActivated && this.renderWalletJauges()}
+        {!isWalletActivated && this.renderActivationLink()}
+      </div>
+    )
+  }
 }
 
 MonPassCulture.propTypes = {

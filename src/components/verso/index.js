5a61ac42ea6f207bcadeabcab36bb52af1f3fd31
sixertoy <sixertoy.github@gmail.com>
2018-10-23 13:11:34 +0200

    💄 update tuto card with activation

--- src/components/verso/index.js
+++ src/components/verso/index.js
@@ -11,18 +11,18 @@ import StaticVerso from './StaticVerso'
 import ActivationCard from './ActivationCard'
 import currentRecommendation from '../../selectors/currentRecommendation'
 
-const Verso = ({ recommendation, isFlipped }) => {
+const Verso = ({ isFlipped, isWalletActivated, recommendation }) => {
   const { mediation } = recommendation || {}
   const { tutoIndex } = mediation || {}
   const isTuto = typeof tutoIndex === 'number' && mediation
-  const isImage = isTuto && tutoIndex !== 1
-  const isActivation = isTuto && tutoIndex === 1
+  const useTutoImage = isTuto && isWalletActivated
+  const useActivation = isTuto && !isWalletActivated && tutoIndex === 1
   return (
     <div className={`verso ${(isFlipped && 'flipped') || ''}`}>
       <VersoWrapper className="with-padding-top">
-        {isImage && <StaticVerso mediationId={mediation.id} />}
-        {isActivation && <ActivationCard mediationId={mediation.id} />}
         {!isTuto && <VersoInfo />}
+        {useTutoImage && <StaticVerso mediationId={mediation.id} />}
+        {useActivation && <ActivationCard mediationId={mediation.id} />}
       </VersoWrapper>
       <Footer borderTop colored={typeof tutoIndex !== 'number'} />
     </div>
@@ -35,6 +35,7 @@ Verso.defaultProps = {
 
 Verso.propTypes = {
   isFlipped: PropTypes.bool.isRequired,
+  isWalletActivated: PropTypes.bool.isRequired,
   recommendation: PropTypes.object,
 }
 
@@ -42,9 +43,11 @@ export default compose(
   withRouter,
   connect((state, ownProps) => {
     const { mediationId, offerId } = ownProps.match.params
+    const isWalletActivated = state.user.wallet_is_activated
     const recommendation = currentRecommendation(state, offerId, mediationId)
     return {
       isFlipped: state.verso.isFlipped,
+      isWalletActivated,
       recommendation,
     }
   })

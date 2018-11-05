ea98d4d5f60b14b978b573760922141bd4d76ae9
sixertoy <sixertoy.github@gmail.com>
2018-11-05 13:09:55 +0100

    🚧 ajout du popperContainer pour permettre l'affichage du calendrier en dehors du form

--- src/components/forms/inputs/index.js
+++ src/components/forms/inputs/index.js
@@ -1,4 +1,7 @@
+// [DEPRECATED] ant legacy version
 export * from './CalendarField'
+// new version
+export { default as DatePickerField } from './DatePickerField'
 export * from './EmailField'
 export * from './HiddenField'
 export * from './InputField'
